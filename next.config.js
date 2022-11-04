7c74f405ed2e5075cf34e060cb55530de9ed0abc
Tinthavong <timainthavong@gmail.com>
2022-11-04 14:14:22 -0400

    next.config

--- next.config.js
+++ next.config.js
@@ -1,10 +1,14 @@
 const isProd = process.env.NODE_ENV === "production";
 
 module.exports = {
-  assetPrefix : isProd ? "/tim-portfolio/" : "",
-     images: {
+  assetPrefix: isProd ? "/tim-portfolio/" : "",
+  images: {
     unoptimized: true,
+    loader: "akami",
+    path: "",
   },
+  basePath: "/tim-portfolio",
+  assetPrefix: "/tim-portfolio",
   reactStrictMode: true,
   swcMinify: true,
 };
