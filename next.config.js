ef4fbbc8b2c6dbcbb563f615f80c21637c57b957
Tinthavong <timainthavong@gmail.com>
2022-11-04 05:34:16 -0400

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -3,5 +3,6 @@ const isProd = process.env.NODE_ENV === "production";
 module.exports = {
   assetPrefix : isProd ? "/tim-portfolio/" : "",
   reactStrictMode: true,
-  swcMinify: true
+  swcMinify: true,
+  images.unoptimized = true
 }
