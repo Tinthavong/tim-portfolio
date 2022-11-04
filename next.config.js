3cbd2fe2e91041b5d4bce4b4838eae1672c2fa95
Tinthavong <timainthavong@gmail.com>
2022-11-04 16:07:06 -0400

    various fixes

--- next.config.js
+++ next.config.js
@@ -1,13 +1,4 @@
-const isProd = process.env.NODE_ENV === "production";
-
 module.exports = {
-  assetPrefix: isProd ? "/tim-portfolio/" : "",
-  images: {
-    loader: "akamai",
-    path: " ",
-  },
-  basePath: "/tim-portfolio",
-  assetPrefix: "/tim-portfolio",
   reactStrictMode: true,
   swcMinify: true,
 };
