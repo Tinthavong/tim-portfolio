6dbedf1ab695346c7907a048d9265ad8cfe4d1d0
Tinthavong <timainthavong@gmail.com>
2022-11-04 04:27:13 -0400

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 const isProd = process.env.NODE_ENV === 'production'
 
 module.exports = {
-  assetPrefix : isProd ? "https://tinthavong.github.io" : '',
+  assetPrefix : isProd ? "/tinthavong.github.io/" : '',
   reactStrictMode: true,
   swcMinify: true
 }
